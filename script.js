42a41647ac9fd4e6f6e0370d9b8469e3849d0498
Your Name <florian.eusterholz@gmail.com>
2024-03-20 18:31:31 +0100

    generate symbols function added

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function init() {
         if (fields[index] === 'circle') {
           symbol = generateCircleSVG();
         } else if (fields[index] === 'cross') {
-          symbol = 'X';
+          symbol = generateCrossSVG();
         }
         tableHTML += `<td>${symbol}</td>`;
       }
@@ -35,6 +35,7 @@ function init() {
     tableHTML += '</table>';
     contentDiv.innerHTML = tableHTML;
 }
+
   
 function generateCircleSVG() {
     const color = '#00B0EF';
@@ -46,6 +47,30 @@ function generateCircleSVG() {
                 <animate attributeName="stroke-dasharray" from="0 188.5" to="188.5 0" dur="0.2s" fill="freeze" />
               </circle>
             </svg>`;
+}
+
+
+function generateCrossSVG() {
+    const color = '#FFC000';
+    const width = 70;
+    const height = 70;
+
+    const svgHtml = `
+      <svg width="${width}" height="${height}">
+        <line x1="0" y1="0" x2="${width}" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="0; ${width}" dur="200ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="200ms" />
+        </line>
+        <line x1="${width}" y1="0" x2="0" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="${width}; 0" dur="200ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="200ms" />
+        </line>
+      </svg>
+    `;
+
+    return svgHtml;
 }
  
   
